59404ae9677c2b12c7422b562ce3d2e65354b095
Aaabbbddds <102460109+Aaabbbddds@users.noreply.github.com>
2022-04-03 19:17:20 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,78 +1,88 @@
-<?php 
-/**
- *Author: Tilon
- *
- *Telegram : @TILON
- */
-$API_KEY = '5102454298:AAFtasDA-yaCJxCjO07uz3eetkvWvaNzPz8';
-##------------------------------##
-define('API_KEY',$API_KEY);
-function bot($method,$datas=[]){
-    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
-    $ch = curl_init();
+<?php
+ob_start();
+error_reporting(0);
+define("bb",'5102454298:AAFtasDA-yaCJxCjO07uz3eetkvWvaNzPz8');
+$admin = "644753244";
+$botname = bot('getme',['bot'])->result->username;
+$botid= bot('getme',['bot'])->result->id;
+
+
+
+function bot($method, $datas=[]){
+	$url = "https://api.telegram.org/bot".bb."/$method";
+	$ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,$url);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas); 
     $res = curl_exec($ch);
     if(curl_error($ch)){
-        var_dump(curl_error($ch));
+    var_dump(curl_error($ch));
     }else{
-        return json_decode($res);
-    }
+    return json_decode($res);
 }
- 
- function sendaction($chat_id, $action){
- bot('sendchataction',[
- 'chat_id'=>$chat_id,
- 'action'=>$action
- ]);
- }
- //====================ᵗᶦᵏᵃᵖᵖ======================//
-$update = json_decode(file_get_contents('php://input'));
-$message = $update->message;
-$from_id = $message->from->id;
-$chat_id = $message->chat->id;
-$t = $message->text;
-$m = $message->message_id;
-$text = $message->text;
-$botname = bot('getme',['bot'])->result->username;
-$u = json_decode(file_get_contents('php://input'));
-//====================ᵗᶦᵏᵃᵖᵖ======================//
+}
 
-if($t=="/start" or $t=="/start@$botname" or $t=="/Start@$botname" or $t=="$botname" or $t=="/Start" or $t=="/START"or $t=="@$botname"){
-bot('sendMessage',[
-'chat_id'=>$chat_id,
-    'message_id'=>$m,
+
+$update = json_decode(file_get_contents("php://input"));
+$message = $update->message;
+$mid = $message->message_id;
+$cid = $message->chat->id;
+$text = $message->text;
+$type = $message->chat->type;
+$title = $message->chat->title;
+$uid = $message->from->id;
+$uid2 = $update->callback_query->from->id;
+$name = $message->chat->first_name;
+$new = $message->new_chat_member;
+$left = $message->left_chat_member;
+$leftid = $message->left_chat_member->id;
+$newid = $message->new_chat_member->id;
+$newname = $message->new_chat_member->first_name;
+
+$g = json_encode([
+'inline_keyboard'=>[
+[['text'=>"➕ Guruhga Qoʻshish",'url'=>"http://t.me/$botname?startgroup=new"]],
+]
+]);
+
+
+if($text == "/start" and $type == "private"){
+	bot('sendmessage',[
+	'chat_id'=>$cid,
 'text'=>"*👋 Salom men guruhlarda kirdi-chiqdi xabarlarni oʻchiruvchi botman!*
 *Istasangiz meni o'z guruhingizga qo'shing ✅*",
 'parse_mode'=>'markdown',
-'reply_markup' => json_encode([
-'inline_keyboard'=>[
-    [['text'=>"➕ Guruhga Qo'shish",'url'=>"http://t.me/$botname?startgroup=new"]],
-]
-])
-]);}
-$message_id = $message->message_id;
-$uid = $message->from->id;
-$new = $message->new_chat_member;
-$left = $message->left_chat_member;
-$newid = $message->new_chat_member->id;
+	'reply_markup'=>$g,
+]);
+}
+
+if($newid == $botid){
+   bot('sendmessage',[
+   'chat_id'=>$cid,
+'text'=>"*👋 Salom men guruhlarda kirdi-chiqdi xabarlarni oʻchiruvchi botman!*
+*Istasangiz meni o'z guruhingizga qo'shing ✅*",
+'parse_mode'=>'markdown',
+   'reply_markup'=>$g,
+]);
+}
+
+//New Chat Member
+
 if($new){
 	if($newid == $uid){
-    send('DeleteMessage',[
-      'message_id'=>$message_id,
-    ]);
-  }
+    bot('DeleteMessage',[
+    'chat_id'=>$cid,
+    'message_id'=>$mid,
+]);
+
+}
 }
 
 if($left){
   bot('DeleteMessage',[
-    'message_id'=>$message_id,
-  ]);
-}if($u->message->leaveChat or $u->message->new_chat_member or $u->message->new_chat_photo or $u->message->new_chat_title or $u->message->left_chat_member or $u->message->pinned_message){
-    bot('deleteMessage',[
-        'chat_id'=>$chat_id,
-        'message_id'=>$m,
-    ]);
+    'chat_id'=>$cid,
+    'message_id'=>$mid,
+]);
 }
 
+?>
